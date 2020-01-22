3a82d183ffec5056aa5c1be50a5f5dfe1c1d9169
lakshan <manualsplash@gmail.com>
2020-01-22 23:36:57 +0530

    Create an endpoint to download the csv file

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hi There')
 })
 
+app.get('/getCSV', (req, res) => {
+    res.download('./out.csv')
+})
+
+
 app.listen(8888, () => {
     console.log('Connected to port 8080')
 })
