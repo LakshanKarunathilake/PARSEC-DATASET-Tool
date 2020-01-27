bcc3ddece78a86f1afaf9f7f11cdd2b694712c20
lakshan <manualsplash@gmail.com>
2020-01-28 00:20:09 +0530

    Prettify app js

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const {createInitialCSV} = require('./CSVHandler')
-const {getDeployments} = require('./TaskRunner')
+const { createInitialCSV } = require("./CSVHandler");
+const { getNamespaces } = require("./TaskRunner");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hi There')
-})
-
-app.get('/getCSV', (req, res) => {
-    res.download('./out.csv')
-})
-
-app.post('/result', function (request, response) {
-    var query1 = request.body.var1;
-    var query2 = request.body.var2;
+app.get("/", (req, res) => {
+  res.send("Hi There");
 });
 
+app.get("/getCSV", (req, res) => {
+  res.download("./out.csv");
+});
+
+app.post("/result", function(request, response) {
+  var query1 = request.body.var1;
+  var query2 = request.body.var2;
+});
 
 app.listen(8888, () => {
-    console.log('Connected to port 8080')
-})
+  console.log("Connected to port 8080");
+});
 
 // createInitialCSV()
-getDeployments()
+getNamespaces();
