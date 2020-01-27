9bdce392c629ba835e687a6445ba5d65f1755c75
lakshan <manualsplash@gmail.com>
2020-01-28 00:20:27 +0530

    Update the kube config

--- TaskRunner.js
+++ TaskRunner.js
@@ -5,6 +5,13 @@ const config = {
     {
       cluster: {
         "certificate-authority-data": "DATA+OMITTED",
+        server: "https://35.188.149.198"
+      },
+      name: "gke_pipeline-concurrency_us-central1-a_parsec-runner"
+    },
+    {
+      cluster: {
+        "certificate-authority-data": "DATA+OMITTED",
         server: "https://104.197.179.49"
       },
       name: "gke_pipeline-concurrency_us-central1-a_test"
@@ -13,15 +20,38 @@ const config = {
   contexts: [
     {
       context: {
+        cluster: "gke_pipeline-concurrency_us-central1-a_parsec-runner",
+        user: "gke_pipeline-concurrency_us-central1-a_parsec-runner"
+      },
+      name: "gke_pipeline-concurrency_us-central1-a_parsec-runner"
+    },
+    {
+      context: {
         cluster: "gke_pipeline-concurrency_us-central1-a_test",
         user: "gke_pipeline-concurrency_us-central1-a_test"
       },
       name: "gke_pipeline-concurrency_us-central1-a_test"
     }
   ],
-  "current-context": "gke_pipeline-concurrency_us-central1-a_test",
+  "current-context": "gke_pipeline-concurrency_us-central1-a_parsec-runner",
   kind: "Config",
+  preferences: {},
   users: [
+    {
+      name: "gke_pipeline-concurrency_us-central1-a_parsec-runner",
+      user: {
+        "auth-provider": {
+          config: {
+            "cmd-args": "config config-helper --format=json",
+            "cmd-path":
+              "C:\\Users\\lakshan\\AppData\\Local\\Google\\Cloud SDK\\google-cloud-sdk\\bin\\gcloud.cmd",
+            "expiry-key": "{.credential.token_expiry}",
+            "token-key": "{.credential.access_token}"
+          },
+          name: "gcp"
+        }
+      }
+    },
     {
       name: "gke_pipeline-concurrency_us-central1-a_test",
       user: {
@@ -42,10 +72,18 @@ const config = {
     }
   ]
 };
-const { KubeConfig } = require("kubernetes-client");
+const { KubeConfig,Client } = require("kubernetes-client");
 const kubeconfig = new KubeConfig();
 kubeconfig.loadFromString(JSON.stringify(config));
 
 const Request = require("kubernetes-client/backends/request");
-const backend = new Request({ kubeconfig });
-const client = new Client({ backend, version: "1.13" });
+
+
+async function getNamespaces() {
+  const backend = new Request({ kubeconfig });
+  const client = new Client({ backend, version: "1.13" });
+  const namespaces = await client.api.v1.namespaces.get();
+  console.log("Namespaces: ", namespaces);
+}
+
+module.exports = { getNamespaces };
