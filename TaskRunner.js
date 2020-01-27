5f5f130bb9a4d399d8baf2ae302cf4a66f44a4fa
lakshan <manualsplash@gmail.com>
2020-01-27 23:52:33 +0530

    Update task runner to run custom configuration

--- TaskRunner.js
+++ TaskRunner.js
@@ -1,17 +1,51 @@
-const { exec } = require("child_process");
-
-
-function runCommands(){
-
-}
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+// Should match the kubeconfig file format exactly
+const config = {
+  apiVersion: "v1",
+  clusters: [
+    {
+      cluster: {
+        "certificate-authority-data": "DATA+OMITTED",
+        server: "https://104.197.179.49"
+      },
+      name: "gke_pipeline-concurrency_us-central1-a_test"
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+  ],
+  contexts: [
+    {
+      context: {
+        cluster: "gke_pipeline-concurrency_us-central1-a_test",
+        user: "gke_pipeline-concurrency_us-central1-a_test"
+      },
+      name: "gke_pipeline-concurrency_us-central1-a_test"
     }
-    console.log(`stdout: ${stdout}`);
-});
+  ],
+  "current-context": "gke_pipeline-concurrency_us-central1-a_test",
+  kind: "Config",
+  users: [
+    {
+      name: "gke_pipeline-concurrency_us-central1-a_test",
+      user: {
+        "auth-provider": {
+          config: {
+            "access-token":
+              "ya29.Ima7B7KdCqHHOoRf9IQ4FdfiWUXArptUqasVrm44IlvfXCV_gPGbPhFeWAS_mviPilqN9BaHf88-lAV-8YFl4ZeWkDFCem0uILQnZLrdw3HZiwye3V_CXxT0Rh025Do_bOCfcjFWAxg",
+            "cmd-args": "config config-helper --format=json",
+            "cmd-path":
+              "C:\\Users\\lakshan\\AppData\\Local\\Google\\Cloud SDK\\google-cloud-sdk\\bin\\gcloud.cmd",
+            expiry: "2020-01-27T18:47:36Z",
+            "expiry-key": "{.credential.token_expiry}",
+            "token-key": "{.credential.access_token}"
+          },
+          name: "gcp"
+        }
+      }
+    }
+  ]
+};
+const { KubeConfig } = require("kubernetes-client");
+const kubeconfig = new KubeConfig();
+kubeconfig.loadFromString(JSON.stringify(config));
+
+const Request = require("kubernetes-client/backends/request");
+const backend = new Request({ kubeconfig });
+const client = new Client({ backend, version: "1.13" });
