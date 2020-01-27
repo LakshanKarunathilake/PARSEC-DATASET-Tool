665902f09e850da4cd707e3d536b67fbe99e8550
lakshan <manualsplash@gmail.com>
2020-01-27 23:52:57 +0530

    Add task runner and simple post endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
 const {createInitialCSV} = require('./CSVHandler')
+const {getDeployments} = require('./TaskRunner')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -14,9 +15,9 @@ app.get('/getCSV', (req, res) => {
     res.download('./out.csv')
 })
 
-app.post('/result',function(request,response){
-    var query1=request.body.var1;
-    var query2=request.body.var2;
+app.post('/result', function (request, response) {
+    var query1 = request.body.var1;
+    var query2 = request.body.var2;
 });
 
 
@@ -25,3 +26,4 @@ app.listen(8888, () => {
 })
 
 // createInitialCSV()
+getDeployments()
