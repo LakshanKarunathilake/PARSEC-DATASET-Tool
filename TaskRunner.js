56c4d7c9de67a7b018b6b375e6bf8cce0a77e896
lakshan <manualsplash@gmail.com>
2020-01-28 23:41:54 +0530

    Add function to post a job and get status of job

--- TaskRunner.js
+++ TaskRunner.js
@@ -14,4 +14,51 @@ async function main() {
   }
 }
 
-main();
+// main();
+
+async function createTask() {
+  const job = {
+    apiVersion: "batch/v1",
+    kind: "Job",
+    metadata: {
+      name: "example-job"
+    },
+    spec: {
+      template: {
+        metadata: {
+          name: "example-job"
+        },
+        spec: {
+          containers: [
+            {
+              name: "pi",
+              image: "perl",
+              command: ["perl"],
+              args: ["-Mbignum=bpi", "-wle", "print bpi(2000)"]
+            }
+          ],
+          restartPolicy: "Never"
+        }
+      }
+    }
+  };
+
+  const client = new Client({ version: "1.9" });
+  const jobCreation = await client.apis.batch.v1
+    .namespaces("parsec")
+    .jobs.post({ body: job });
+  console.log("done");
+  console.log("job", jobCreation);
+}
+
+async function getStatusOfJob() {
+  const client = new Client({ version: "1.9" });
+  const jobCreation = await client.apis.batch.v1
+    .namespaces("parsec")
+    .jobs("example-job")
+    .status.get();
+  console.log("status", jobCreation);
+}
+
+// createTask()
+getStatusOfJob();
