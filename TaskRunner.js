7f53e9c8e121f5fcd824816519131231b115d850
lakshan <manualsplash@gmail.com>
2020-02-02 00:35:33 +0530

    Add a way to obtain logs

--- TaskRunner.js
+++ TaskRunner.js
@@ -1,73 +1,98 @@
 const Client = require("kubernetes-client").Client;
-const fs = require('fs');
+const fs = require("fs");
+const { exec } = require("child_process");
 
 /**
  * Reading from the csv file that includes the combinations of the
  */
-function traversInParameterCombination() {
-
-    // Reading from the csv file
-    fs.readFile('parameter-combination.json', (err, data) => {
-        if (err) {
-            console.log('Error reading json file', err)
-        }
-        ;
-        let combinations = JSON.parse(data);
-        Object.values(combinations).forEach( async combination  => {
-            await createTask(combination);
+async function traversInParameterCombination() {
+  // Reading from the csv file
+  fs.readFile("parameter-combination.json", (err, data) => {
+    if (err) {
+      console.log("Error reading json file", err);
+    }
+    let combinations = JSON.parse(data);
+    Object.values(combinations).forEach(combination => {
+      createTask(combination)
+        .then(() => {
+          console.log("Done");
+          // getLogsOfJob(combination.id);
+        })
+        .catch(err => {
+          console.log("Error in Job Creating", err);
         });
     });
+  });
 }
 
-async function createTask({name, input, compiler, threads, cores}) {
-    const job = {
-        "apiVersion": "batch/v1",
-        "kind": "Job",
-        "metadata": {
-            "name": `${name}-${compiler}-${input}-${cores}-${threads}`
-        },
-        "spec": {
-            "template": {
-                "spec": {
-                    "containers": [
-                        {
-                            "name": "parsec-3-0",
-                            "image": "spirals/parsec-3.0",
-                            "resources": {
-                                "limits": {
-                                    "cpu": cores
-                                }
-                            },
-                            "imagePullPolicy": "IfNotPresent",
-                            "command": [
-                                "-a", "run", "-p", name, "-c", compiler, "-i", input, "-t", threads
-                            ]
-                        }
-                    ],
-                    "restartPolicy": "Never"
+async function createTask({ name, input, compiler, threads, cores, id }) {
+  const job = {
+    apiVersion: "batch/v1",
+    kind: "Job",
+    metadata: {
+      name: `job-${id}`
+    },
+    spec: {
+      template: {
+        spec: {
+          containers: [
+            {
+              name: "pi",
+              image: "perl",
+              command: ["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"],
+              resources: {
+                limits: {
+                  cpu: "1"
+                },
+                requests: {
+                  cpu: "0.5"
                 }
-            },
-            "backoffLimit": 2
+              }
+            }
+          ],
+          restartPolicy: "Never"
         }
+      },
+      backoffLimit: 4
     }
-
-    const client = new Client({ version: "1.13" });
-    // await client.api.v1.namespaces.post({body:'parsec'})
-    const jobCreation = await client.apis.batch.v1
-      .namespaces("default")
-      .jobs.post({ body: job });
-  console.log('jobcreation',jobCreation)
+  };
+  console.log("job", job);
+  const client = new Client({ version: "1.9" });
+  // await client.api.v1.namespaces.post({body:'parsec'})
+  await client.apis.batch.v1.namespaces("default").jobs.post({ body: job });
 }
 
 async function getStatusOfJob() {
-    const client = new Client({version: "1.9"});
-    const jobCreation = await client.apis.batch.v1
-        .namespaces("parsec")
-        .jobs("example-job")
-        .status.get();
-    console.log("status", jobCreation);
+  const client = new Client({ version: "1.9" });
+  const jobCreation = await client.apis.batch.v1
+    .namespaces("parsec")
+    .jobs("example-job")
+    .status.get();
+  console.log("status", jobCreation);
+}
+
+function getLogsOfJob(id) {
+  const client = new Client({ version: "1.9" });
+  client.apis.batch.v1
+    .namespaces("default")
+    .jobs(`job-${id}`)
+    .status.get()
+    .then(data => {
+      console.log("status", data);
+      exec(`kubectl logs jobs/job-${id}`, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+    });
 }
 
 module.exports = {
-    traversInParameterCombination
-}
+  traversInParameterCombination
+};
