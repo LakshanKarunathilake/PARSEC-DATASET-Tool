d89174f132c6b86311b970d4292724c507acd8e8
Lakshan Karunathilake <manualsplash@gmail.com>
2020-02-01 21:07:21 +0530

    Update task runner

--- TaskRunner.js
+++ TaskRunner.js
@@ -1,91 +1,73 @@
 const Client = require("kubernetes-client").Client;
+const fs = require('fs');
 
-async function main() {
-  try {
-    const client = new Client({ version: "1.9" });
+/**
+ * Reading from the csv file that includes the combinations of the
+ */
+function traversInParameterCombination() {
 
-    //
-    // Get all the Namespaces.
-    //
-    const namespaces = await client.api.v1.pods.get();
-    console.log("Namespaces: ", namespaces);
-  } catch (err) {
-    console.error("Error: ", err);
-  }
+    // Reading from the csv file
+    fs.readFile('parameter-combination.json', (err, data) => {
+        if (err) {
+            console.log('Error reading json file', err)
+        }
+        ;
+        let combinations = JSON.parse(data);
+        Object.values(combinations).forEach( async combination  => {
+            await createTask(combination);
+        });
+    });
 }
 
-main();
-
-async function createTask() {
-  // const job = {
-  //   apiVersion: "batch/v1",
-  //   kind: "Job",
-  //   metadata: {
-  //     name: "example-job"
-  //   },
-  //   spec: {
-  //     template: {
-  //       metadata: {
-  //         name: "example-job"
-  //       },
-  //       spec: {
-  //         containers: [
-  //           {
-  //             name: "pi",
-  //             image: "perl",
-  //             command: ["perl"],
-  //             args: ["-Mbignum=bpi", "-wle", "print bpi(2000)"]
-  //           }
-  //         ],
-  //         restartPolicy: "Never"
-  //       }
-  //     }
-  //   }
-  // };
-  const job = {
-    "apiVersion": "batch/v1",
-    "kind": "Job",
-    "metadata": {
-      "name": "parsec-job-1"
-    },
-    "spec": {
-      "template": {
+async function createTask({name, input, compiler, threads, cores}) {
+    const job = {
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata": {
+            "name": `${name}-${compiler}-${input}-${cores}-${threads}`
+        },
         "spec": {
-          "containers": [
-            {
-              "name": "parsec-3-0",
-              "image": "spirals/parsec-3.0",
-              "imagePullPolicy": "IfNotPresent",
-              "command": [
-                // "spirals/parsec-3.0", "-S", "parsec", "-a", "run" ,"-p" ,"dedup",
-                  "ls"
-              ]
-            }
-          ],
-          "restartPolicy": "Never"
+            "template": {
+                "spec": {
+                    "containers": [
+                        {
+                            "name": "parsec-3-0",
+                            "image": "spirals/parsec-3.0",
+                            "resources": {
+                                "limits": {
+                                    "cpu": cores
+                                }
+                            },
+                            "imagePullPolicy": "IfNotPresent",
+                            "command": [
+                                "-a", "run", "-p", name, "-c", compiler, "-i", input, "-t", threads
+                            ]
+                        }
+                    ],
+                    "restartPolicy": "Never"
+                }
+            },
+            "backoffLimit": 2
         }
-      },
-      "backoffLimit": 2
     }
-  }
 
-  const client = new Client({ version: "1.9" });
-  // await client.api.v1.namespaces.post({body:'parsec'})
-  const jobCreation = await client.apis.batch.v1
-    .namespaces("default")
-    .jobs.post({ body: job });
-  console.log("done");
-  console.log("job", jobCreation);
+    const client = new Client({ version: "1.13" });
+    // await client.api.v1.namespaces.post({body:'parsec'})
+    const jobCreation = await client.apis.batch.v1
+      .namespaces("default")
+      .jobs.post({ body: job });
+  console.log('jobcreation',jobCreation)
 }
 
 async function getStatusOfJob() {
-  const client = new Client({ version: "1.9" });
-  const jobCreation = await client.apis.batch.v1
-    .namespaces("parsec")
-    .jobs("example-job")
-    .status.get();
-  console.log("status", jobCreation);
+    const client = new Client({version: "1.9"});
+    const jobCreation = await client.apis.batch.v1
+        .namespaces("parsec")
+        .jobs("example-job")
+        .status.get();
+    console.log("status", jobCreation);
 }
 
-createTask()
-// getStatusOfJob();
+module.exports = {
+    traversInParameterCombination
+}
