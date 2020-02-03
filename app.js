7a8b8e39704762a780ebc787e4f9a3f4e8d12a00
Lakshan Karunathilake <manualsplash@gmail.com>
2020-02-03 12:18:26 +0530

    Revoke token at every 1 min

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const { createInitialJSON } = require("./JSONHandler");
-const {traversInParameterCombination} =require("./TaskRunner");
+const { traversInParameterCombination } = require("./TaskRunner");
+const { exec } = require("child_process");
 
-createInitialJSON()
-    .then(()=>{
-        traversInParameterCombination()
-    })
+createInitialJSON().then(() => {
+  traversInParameterCombination();
+});
 
+setInterval(() => {
+  exec("kubectl cluster-info", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}, 60000);
