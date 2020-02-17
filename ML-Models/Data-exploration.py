28faa98f549420afd496a3d223a9479c0c78847f
lakshan <manualsplash@gmail.com>
2020-02-17 23:12:01 +0530

    Convert time durations

--- ML-Models/Data-exploration.py
+++ ML-Models/Data-exploration.py
@@ -9,8 +9,23 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_csv('results.csv')
+
+# Printing the sample head
 print(data.head())
-for col in data['real']: 
-    print(col) 
 
 
+def convertExecutionTime(time):
+    MIN_TO_MIL_SECONDS = 60000
+    SECONDS_TO_MIL_SECONDS = 1000
+    min,seconds = time.split('m')
+    seconds = seconds.replace('s','')
+    min = int(min) 
+    seconds = float(seconds)
+    fullTimeinMilliseconds = int(min * MIN_TO_MIL_SECONDS + seconds * SECONDS_TO_MIL_SECONDS) 
+    return fullTimeinMilliseconds
+
+data['real'] = data['real'].apply(convertExecutionTime)
+data['usr'] = data['usr'].apply(convertExecutionTime)
+data['sys'] = data['sys'].apply(convertExecutionTime)
+
+print(data.describe())
